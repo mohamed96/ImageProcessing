2abfa84c0e376df44b078b975ab890dc0c46bdbf
mohamed96 <momocuri96@gmail.com>
2016-07-16 20:23:34 +0200

    Add files via upload

---
+++ test/ImageProcessingTest.cpp
@@ -0,0 +1,22 @@
+//
+// Created by mohamed on 15/07/16.
+//
+
+#include "gtest/gtest.h"
+#include "../ImageProcessing.h"
+
+
+TEST(ImageProcessingTest, BlurTest) {
+    Image *image=new Image(4, 4);
+    for(int i=0;i<image->width;i++)
+        for(int j=0;j<image->height;j++)
+            image->image[i*image->width+j]=(i*image->width+j);
+    ImageProcessing<RGBPixel> processing(image);
+    processing.blur();
+    ASSERT_EQ(image->image[1*image->width+1].getR(), 5);
+    ASSERT_EQ(image->image[1*image->width+1].getG(), 5);
+    ASSERT_EQ(image->image[1*image->width+1].getB(), 5);
+    ASSERT_EQ(image->image[1*image->width+2].getR(), 6);
+    ASSERT_EQ(image->image[1*image->width+2].getG(), 6);
+    ASSERT_EQ(image->image[1*image->width+2].getB(), 6);
+}
